343cfbe4e4951f5deba70ef142f1abda15f9c3d3
Eloi Morais <eloi_matos@hotmail.com>
2020-05-23 20:37:35 -0300

    Add util classes

---
+++ src/entities/Size.java
@@ -0,0 +1,27 @@
+package entities;
+
+public class Size {
+    private int height;
+    private int width;
+
+    public Size(int width, int height) {
+        this.height = height;
+        this.width = width;
+    }
+
+    public int getHeight() {
+        return height;
+    }
+
+    public int getWidth() {
+        return width;
+    }
+
+    public void setHeight(int height) {
+        this.height = height;
+    }
+
+    public void setWidth(int width) {
+        this.width = width;
+    }
+}
