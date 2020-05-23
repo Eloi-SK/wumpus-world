24676f3e16e452c816d64895b4daa451ca28c01d
Eloi Morais <eloi_matos@hotmail.com>
2020-05-23 20:44:06 -0300

    Get adjacent cells

--- src/entities/Cave.java
+++ src/entities/Cave.java
@@ -17,4 +17,28 @@ public class Cave {
             System.out.println(Arrays.toString(row));
     }
 
+    public ArrayList<Position> getAdjacentCells(Position position) {
+        ArrayList<Position> adjacentCells = new ArrayList<Position>();
+        
+        int x = position.getX();
+        int y = position.getY();
+
+        if (y - 1 >= 0) {
+            adjacentCells.add(new Position(x, y - 1));
+        }
+
+        if (y + 1 <= this.size.getHeight()) {
+            adjacentCells.add(new Position(x, y + 1));
+        }
+
+        if (x - 1 >= 0) {
+            adjacentCells.add(new Position(x - 1, y));
+        }
+
+        if (x + 1 <= this.size.getWidth()) {
+            adjacentCells.add(new Position(x + 1, y));
+        }
+
+        return adjacentCells;
+    }
 }
